4d4c6a3c3a994532bc31168d208ba8aefd913a74
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-27 14:23:41 +0530

    Added: Calculate monthly employee wage till condition of total working hours or days is reached for a month

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -7,10 +7,12 @@
     $empHours;
     $workingDaysPerMonth = 20;
     $numOfWorkingDays = 1;
+    $totalWorkingHours = 100;
+    $totalEmpHours = 0;
     $monthlyEmpWage = 0;
 
     //To calculate wage for a month
-    while($numOfWorkingDays <= $workingDaysPerMonth){
+    while($numOfWorkingDays <= $workingDaysPerMonth && $empHours <= $totalWorkingHours){
         $num = rand(0,2); //Generating random numbers 0 and 2
         //Condition to get empHours i.e part time, full time or absent
         switch($num){
@@ -28,6 +30,7 @@
                 break;
         }
         $numOfWorkingDays++;
+        $totalEmpHours += $empHours;
         //Calculate Daily Employee Wage
         $dailyEmpWage = $wagePerHour * $empHours;
         $monthlyEmpWage +=$dailyEmpWage;
